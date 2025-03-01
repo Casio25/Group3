a662d0968ed0ec7b12021a94d57abbbc7732a996
Casio25 <mishakolomiets355@gmail.com>
2025-03-01 20:43:25 +0200

    workflow fix

--- main.tf
+++ main.tf
@@ -2,40 +2,23 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+# Отримуємо існуючу групу безпеки
+data "aws_security_group" "allow_http_ssh" {
+  filter {
+    name   = "group-name"
+    values = ["allow_http_ssh"]
+  }
+}
+
+# Створюємо інстанс з використанням існуючої групи безпеки
 resource "aws_instance" "web" {
   ami           = "ami-02912a5f3748ebfac"
   instance_type = "t3.micro"
   key_name      = "group3key"
 
-  security_groups = ["allow_http_ssh"]
+  vpc_security_group_ids = [data.aws_security_group.allow_http_ssh.id]
 
   tags = {
     Name = "Terraform-Managed-Instance"
-  }
-}
-
-resource "aws_security_group" "allow_http_ssh" {
-  name        = "allow_http_ssh"
-  description = "Security group that allows SSH and HTTP access"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
