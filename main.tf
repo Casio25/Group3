6695e8c14b88a2e5552e366d0823436c24cff221
Casio25 <mishakolomiets355@gmail.com>
2025-03-09 18:51:59 +0200

    update main.tf

--- main.tf
+++ main.tf
@@ -2,51 +2,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# 🔹 Створюємо Security Group
-resource "aws_security_group" "allow_http_ssh" {
-  name        = "allow_http_ssh"
-  description = "Security group that allows SSH and HTTP access"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 🔴 Дозволяє SSH для всіх (можна обмежити)
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 🔴 Дозволяє HTTP для всіх
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 🔴 Дозволяє доступ на 8080
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # 🔴 Дозволяє весь вихідний трафік
-  }
-
-  tags = {
-    Name = "allow_http_ssh"
+# Отримуємо існуючу Security Group
+data "aws_security_group" "allow_http_ssh" {
+  filter {
+    name   = "group-name"
+    values = ["allow_http_ssh"]  # Вже існуюча група
   }
 }
 
-# 🔹 Створюємо інстанс з використанням нової Security Group
+# Створюємо EC2-інстанс, використовуючи знайдену Security Group
 resource "aws_instance" "web" {
   ami           = "ami-02912a5f3748ebfac"
   instance_type = "t3.micro"
   key_name      = "group3key"
 
-  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]  # Використовуємо створену SG
+  vpc_security_group_ids = [data.aws_security_group.allow_http_ssh.id]
 
   tags = {
     Name = "Terraform-Managed-Instance"
