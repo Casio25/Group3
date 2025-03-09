942a9da073dcd5ceb85772a0004b006aa683f9ed
Casio25 <mishakolomiets355@gmail.com>
2025-03-09 18:15:03 +0200

    maintf change

--- main.tf
+++ main.tf
@@ -2,20 +2,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# Отримуємо існуючу групу безпеки
+# Отримуємо існуючу Security Group за її ім'ям
 data "aws_security_group" "allow_http_ssh" {
   filter {
     name   = "group-name"
-    values = ["allow_http_ssh"]
+    values = ["launch-wizard-1"]  # Замініть на реальну групу з `aws ec2 describe-security-groups`
   }
 }
 
-# Створюємо інстанс з використанням існуючої групи безпеки
+# Створюємо EC2-інстанс, використовуючи існуючу Security Group
 resource "aws_instance" "web" {
   ami           = "ami-02912a5f3748ebfac"
   instance_type = "t3.micro"
   key_name      = "group3key"
 
+  # Використовуємо знайдену Security Group
   vpc_security_group_ids = [data.aws_security_group.allow_http_ssh.id]
 
   tags = {
